48024cfcec18d1172f4969066effe9ac1775083a
Phoenix181 <aadhishrm11@gmail.com>
2020-02-15 23:48:16 +0530

    Refactoring the rock-paper-scissors code`

--- index.js
+++ index.js
@@ -60,31 +60,57 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function renderScores() {
+    const playerScoreElement = document.getElementById('playerScore');
+    playerScoreElement.innerHTML = playerScore;
+    
+    const computerScoreElement = document.getElementById('computerScore');
+    computerScoreElement.innerHTML = computerScore;
+}
+
+function reset() {
+    playerScore = computerScore = 0;
+    
+    renderScores();
+    const roundMessage = document.getElementById('round-message');
+    roundMessage.innerHTML = '';
+
+    const winMessage = document.getElementById('match-message');
+    winMessage.innerHTML = ''
+}
 
 function game(e) {
     const playerSelection = e.target.id;
     const computerSelection = computerPlay();
     const winner = playRound(playerSelection, computerSelection);
 
-    const winMessage = document.getElementById('message');
-    winMessage.innerHTML='';
-
-    if (winner > 0)
+    const roundMessage = document.getElementById('round-message');
+    roundMessage.innerHTML='';
+    
+    if (winner > 0) {
         playerScore++;
-    else if (winner < 0)
-        computerScore++;
-    else {
-        winMessage.innerHTML='The round is drawn';
+        roundMessage.innerHTML=`You chose ${playerSelection} and computer chose ${computerSelection}. You won.`
     }
-
-    const playerScoreElement = document.getElementById('playerScore');
-    playerScoreElement.innerHTML = playerScore;
-
-    const computerScoreElement = document.getElementById('computerScore');
-    computerScoreElement.innerHTML = computerScore;
+    else if (winner < 0) {
+        roundMessage.innerHTML = `You chose ${playerSelection} and computer chose ${computerSelection}. You lose.`
+        computerScore++;
+    }
+    else {
+        roundMessage.innerHTML = `You chose ${playerSelection} and computer also chose ${computerSelection}. The round is drawn.`;
+    }
+    
+    renderScores();
+    
+    const winMessage = document.getElementById('match-message');
 
     if (playerScore >= 5)
         winMessage.innerHTML = `You win! Score: ${playerScore} : ${computerScore}`;
     else if (computerScore >= 5)
         winMessage.innerHTML = `You lose! Score: ${computerScore} : ${playerScore}`;
+
+    if(playerScore >= 5 || computerScore >= 5) {
+        setTimeout(reset, 5000);
+        const message = document.getElementById('match-message');
+        message.innerHTML = `${message.innerHTML}. \nThe match will reset in 5 seconds...`;
+    }
 }
